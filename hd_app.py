e968360bab7a7025b91f2c7bdd7d870791900e5c
humra024 <72643236+humra024@users.noreply.github.com>
2024-06-17 03:16:06 +0530

    Update hd_app.py

--- hd_app.py
+++ hd_app.py
@@ -37,7 +37,7 @@ def main():
     """
     st.markdown(html_temp, unsafe_allow_html=True)
     
-    #url = 'https://public.tableau.com/shared/MB93XXTDN?:display_count=n&:origin=viz_share_link'
+    url = 'https://public.tableau.com/shared/MB93XXTDN?:display_count=n&:origin=viz_share_link'
 
     if st.button('Heart Disease Dashboard'):
         webbrowser.open_new_tab(url)
